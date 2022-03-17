212ee3d8794dd7649b1eefb16c5b48df309dddb4
Victoria Warren <93948407+vmw19@users.noreply.github.com>
2022-03-17 14:50:25 -0400

    Pet Salon
    Pet Salon Update

--- scripts/register.js
+++ scripts/register.js
@@ -1,36 +1,123 @@
 //create the constructor
-function Pet(name, age, gender, breed, service, ownerName, contactPhone){
-    this.petName=name
-    this.petAge=age
-    this.petGener=gender
-    // this.petNBreed=breed
-    // this.petService=service
-    // this.petownerName=ownerName
-    // this.petcontactPhone=contactPhone
-
+let x = 0;
+function Pet(name, age, gender, breed, service, ownerName, contactPhone) {
+    this.petName = name;
+    this.petAge = age;
+    this.petGender = gender;
+    this.id = x++;
+    this.petBreed=breed;
+    this.petService=service
+    this.petownerName=ownerName
+    this.petcontactPhone=contactPhone
 }
 //create some pets
-let Scooby=new Pet("Scooby", 90, "Male");
-let Cujo=new Pet("Cujo", 45, "Female");
-let Tiny=new Pet("Tiny", 21, "Female");
+let Scooby = new Pet("Scooby", 90, "Male", "Pit", "Nail Trim", "Victoria", 155535685);
+let Cujo = new Pet("Cujo", 45, "Female", "Poodle", "Bath", "Kaleb", 12222224555);
+let Tiny = new Pet("Tiny", 21, "Female", "Bulldog", "Nail Trim", "Joshua",1235913166 );
 petSalon.pets.push(Scooby, Cujo, Tiny);
 
 
 //get the info from the inputs
-let inputPetName=document.getElementById("txtPetName");
-let inputPetAge=document.getElementById("txtPetAge");
-let inputPetGender=document.getElementById("txtPetGender");
- let inputPetBreed=document.getElementById("txtPetBreed");
-// let inputPetService=document.getElementById("txtPetService");
-// let inputPetOwnerName=document.getElementById("txtPetOwerName");
-// let inputPetOwnerPhone=document.getElementById("txtPetOwnerPhone");
+let inputPetName = document.getElementById("txtPetName");
+let inputPetAge = document.getElementById("txtPetAge");
+let inputPetGender = document.getElementById("txtPetGender");
+let inputPetBreed = document.getElementById("txtPetBreed");
+let inputPetService = document.getElementById("txtPetService");
+let inputPetOwnerName = document.getElementById("txtPetOwerName");
+let inputPetOwnerPhone = document.getElementById("txtPetOwnerPhone");
 
-function register(){
+function register() {
     console.log(inputPetName.value, inputPetAge.value, inputPetGender.value);
     //create the objects
-    let thePet = new Pet (inputPetName.value, inputPetAge.value, inputPetGender.value,inputPetBreed.value,inputPetService.value,inputPetOwnerName.value,inputPetOwnerPhone.value,);
+    let thePet = new Pet(inputPetName.value, inputPetAge.value, inputPetGender.value, inputPetBreed.value, inputPetService.value, inputPetOwnerName.value, inputPetOwnerPhone.value, );
     console.log(thePet);
     //push the objects
     petSalon.pets.push(thePet);
     //display the objects in the console
+    displayPetsTable();
 }
+function displayPets() {
+    //travel the array
+    let tmp = "";
+    for (let i = 0; i < petSalon.pets.length; i++) {
+        //create the template
+        // let newP = document.createElement("p");
+        // console.log(newP);
+        // let newText = document.createTextNode(`$petSalon.pets[i].petName`);
+        // console.log(newtext);
+        // newP.appendChild(newText);
+        tmp += `
+    <p> Name: ${petSalon.pets[i].petName}<p>
+    <p> Age: ${petSalon.pets[i].petAge}<p>
+    <p> Gender: ${petSalon.pets[i].petGender}<p>
+    <p> Breed: ${petSalon.pets[i].petBreed}<p>
+    <p> Service: ${petSalon.pets[i].petService}<p>
+    <p> PetOwnerName: ${petSalon.pets[i].petOwnerName}<p>
+    <p> PetOwnerPhone: ${petSalon.pets[i].petOwnerPhone}<p>
+    `;
+        console.log(tmp);
+    }
+    document.getElementById("pets").innerHTML = tmp;
+    //display the tmp on the html
+}
+function displayPetsTable() {
+    //travel the array
+    let row = "";
+    for (let i = 0; i < petSalon.pets.length; i++) {
+        //create the template
+        row+= `
+        <tr id=${petSalon.pets}>
+            <td>  ${petSalon.pets[i].petName}</td>
+            <td> ${petSalon.pets[i].petAge}</td>
+            <td>${petSalon.pets[i].petGender}</td>
+            <td>${petSalon.pets[i].petBreed}</td>
+            <td> ${petSalon.pets[i].petService}/td>
+             <td>${petSalon.pets[i].petOwnerName}</td>
+             <td> ${petSalon.pets[i].petOwnerPhone}</td>
+            <td><button onclick="deletePet($petSalon.pets[i].id});"> Delete</button></td>
+        </tr>
+        `;
+    }
+    document.getElementById("petTable").innerHTML = row;
+    //display the tmp on the html
+        }
+    function deletePet(petId){
+        console.log("Delete pet", petId);
+        for(let i=0;i<petSalon.pets.length, i++){
+            let pet=petSalon.pets[i];
+            if(pet.id==petId){
+                deleteIndex=i;
+                console.log("I found the pet...."deleteIndex);
+            }
+        }   
+    }
+    //remove the pet from the array
+    petSalon.pets.splice(deleteIndex,1);
+    // remove the pet from the html
+        document.getElementById(petId).remove();
+    }
+
+}function searchPet(){
+    let searchString=document.getElementById("txtSearch").value;
+    console.log(searchString);
+    for(let i=0;i<petSalon.pets.length,i++){
+        let pet=petSalon.pets[i];
+        if(pet.petName.toLowerCase()==searchString.toLowerCase()|| pet.petGender.toLowercase())==searchStroing.toLowerCase()){
+            console.log("I found it");
+            document.getElementById(pet.id).classList.add('bg-color');
+        }
+        else{
+            document.getElementById(pet.id).classList.remove('bg-color');
+        }
+    }    
+}
+    function init() {
+        //create some pets
+        let scooby = new Pet("Scooby", 90, "Male");
+        let cujo = new Pet("Cujo", 45, "Female");
+        let tiny = new Pet("Tiny", 21, "Female");
+        petSalon.pets.push(Scooby, Cujo, Tiny)
+        displayPets();
+        displayPetsTable();
+    }
+    window.onload=init;
